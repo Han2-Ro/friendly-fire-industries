282f01a9dfc55d9ebb60ee432dd209a98c0f9e58
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-02-16 15:49:37 +0100

    fix hit

--- scripts/target.gd
+++ scripts/target.gd
@@ -1,11 +1,15 @@
-extends MeshInstance3D
+extends Node3D
 class_name Target
 
 var in_ray: bool
+var area: Area3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	area = find_child("Area3D") as Area3D
+	print(area)
+	area.area_entered.connect(_on_area_3d_area_entered)
+	area.area_exited.connect(_on_area_3d_area_exited)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,11 +21,13 @@ func _input(event: InputEvent) -> void:
 		on_hit()
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
+	print("area entered")
 	if area.is_in_group("ray"):
 		in_ray = true
 
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
+	print("area exited")
 	if area.is_in_group("ray"):
 		in_ray = false
 
