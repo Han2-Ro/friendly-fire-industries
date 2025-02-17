6fd1cb54bcbec0df8b3f33621b4523a9fd15a825
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-02-17 16:53:16 +0100

    add main.gd, reset on level finish

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var speed = 3
+@export var goal_distance = 30
 
 # Called when the node enters the scene tree for the first time.
 @onready var rotation_stange = $Player/player_base/player_rotationstange
@@ -11,6 +12,8 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	position.x += speed * delta
+	if (position.x > goal_distance):
+		get_tree().reload_current_scene()
 
 func _physics_process(_delta: float) -> void:
 	look_at_cursor()
@@ -23,7 +26,7 @@ func look_at_cursor():
 	var from = camera.project_ray_origin(mouse_position)
 	var dir = camera.project_ray_normal(mouse_position)
 	if (dir.z == 0):
-		return;
+		return ;
 		
 	var cursor_position_on_plane = target_plane.intersects_ray(from, dir)
 	if (cursor_position_on_plane != null):
