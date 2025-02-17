df32bb9240d3a6e383b9fad163cae24ab746cbcd
anybuddy9001 <fuaw.gc@lst.de>
2025-02-17 15:19:27 +0100

    basic raycast

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,13 +10,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x += speed * delta
+
+func _physics_process(_delta: float) -> void:
 	look_at_cursor()
 
 func look_at_cursor():
 	var camera := get_viewport().get_camera_3d()
-	var target_plane_mouse := Plane(Vector3(0, 1, 0), position.y)
+	var target_plane := Plane(Vector3(0, 1, 0), position.y)
+	
 	var mouse_position = get_viewport().get_mouse_position()
 	var from = camera.project_ray_origin(mouse_position)
 	var dir = camera.project_ray_normal(mouse_position)
-	var cursor_position_on_plane = target_plane_mouse.intersects_ray(from, dir)
-	look_at(cursor_position_on_plane, Vector3.UP, 0)
+	if (dir.z == 0):
+		return;
+		
+	var cursor_position_on_plane = target_plane.intersects_ray(from, dir)
+	#print(str(from) + " : " + str(dir))
+	if (cursor_position_on_plane != null):
+		look_at(cursor_position_on_plane, Vector3.UP, 0)
+	else:
+		look_at(global_position + Vector3(0,10,0), Vector3.UP, 0)
