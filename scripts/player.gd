d067d107a1db2a6bf29e1918c64b5b8156d2347c
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-02-18 12:08:49 +0100

    player follow path

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,6 @@
-extends Node3D
+extends PathFollow3D
 
 @export var speed = 3
-@export var goal_distance = 30
 
 @onready var rotation_stange = $Player/player_base/player_rotationstange
 
@@ -9,13 +8,11 @@ func _ready() -> void:
 	EventBus.level_end.connect(_on_level_end)
 	
 
-func _process(delta: float) -> void:
-	position.x += speed * delta
-	if (position.x > goal_distance): # TODO: How to do it for more complex path
-		EventBus.level_end.emit(true)
-
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	look_at_cursor()
+	progress += speed * delta
+	if (progress_ratio >= 1.0):
+		EventBus.level_end.emit(true)
 
 func look_at_cursor():
 	var camera := get_viewport().get_camera_3d()
@@ -29,6 +26,7 @@ func look_at_cursor():
 		
 	var cursor_position_on_plane = target_plane.intersects_ray(from, dir)
 	if (cursor_position_on_plane != null):
+		cursor_position_on_plane.y = rotation_stange.global_position.y
 		rotation_stange.look_at(cursor_position_on_plane, Vector3.UP, 0)
 
 func _on_level_end(_success: bool) -> void:
