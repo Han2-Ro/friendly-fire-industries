e661d1edc747ae343705426b2bef85da8f26b2cc
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-02-18 17:37:43 +0100

    feat: minenwerfer in level 2

--- scripts/minenwerfer.gd
+++ scripts/minenwerfer.gd
@@ -4,13 +4,9 @@ extends Node3D
 @export var mine_speed: float = 10
 @export var incline: float = 1
 @export var targets: Array[Node3D]
-@export var seconds_between_shots: float = 5
+@export var seconds_between_shots: float = 3
 var time_since_last_shot: float = 0
 var i_target: int = 0
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("shoot"):
-		launch_mine()
 
 func _physics_process(delta: float) -> void:
 	time_since_last_shot += delta
@@ -19,11 +15,14 @@ func _physics_process(delta: float) -> void:
 		time_since_last_shot = 0
 
 func launch_mine():
+	print("minenwerfer:", global_position)
+	if targets.size() == 0:
+		printerr("Minenwerfer: No targets.")
+		return
 	var mine := mine_scene.instantiate() as Mine
 	mine.target = targets[i_target]
 	mine.speed = mine_speed
 	mine.incline = incline
-	mine.global_position = global_position
 	add_child(mine)
 	i_target += 1
 	if i_target >= targets.size():
