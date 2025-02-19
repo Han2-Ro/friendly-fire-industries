8bb28d671a5d279e3de153fe7213cf5c96ed2f2e
anybuddy9001 <fuaw.gc@lst.de>
2025-02-19 18:19:07 +0100

    rocketsilo as replacement for minenwerfer

---
+++ scripts/rocket.gd
@@ -0,0 +1,50 @@
+extends Node3D
+class_name Rocket
+
+@onready var kill_area: Area3D = $KillArea
+@onready var explodeparticle = preload("res://scenes/Particles/explosion.tscn")
+@onready var model = $Rocket
+
+var target: Vector3
+var speed: float
+var distance: float
+var incline: float
+var x: float = 0
+
+var old_pos: Vector3
+
+func _ready() -> void:
+	old_pos = global_position
+	distance = global_position.distance_to(target)
+	move_mine(0.001)
+
+func _process(delta: float) -> void:
+	if position.y > 0:
+		move_mine(delta)
+	if position.y < 0:
+		global_position = target
+		#rotation_degrees.x = -90
+
+func move_mine(delta: float):
+	var dir = (target - global_position)
+	dir.y = 0
+	dir = dir.normalized()
+	x += speed * delta
+	global_position += (dir * speed * delta)
+	position.y = incline / distance * (x + 0.25) * (distance - x) # Parabolic formula
+	
+	#rotate
+	var flight_dir = (global_position - old_pos).normalized()
+	look_at(global_position + flight_dir, Vector3.UP, 0)
+	old_pos = global_position
+
+func on_hit():
+	var explosion_instance = explodeparticle.instantiate()
+	explosion_instance.global_transform = self.global_transform
+	get_parent().add_child(explosion_instance)
+	explosion_instance.explode()
+	var bodies = kill_area.get_overlapping_bodies()
+	for body in bodies:
+		if body.has_method("on_hit"):
+			body.on_hit()
+	queue_free()
---
+++ scripts/rocketsilo.gd
@@ -0,0 +1,68 @@
+extends StaticBody3D
+
+@export var explosive_speed: float = 10
+@export var incline: float = 10
+@export var targets: Array[Node3D]
+@export var seconds_between_shots: float = 3
+@export var seconds_being_open: float = 2
+
+@onready var AnimPlayer = $AnimationPlayer
+@onready var explodeparticle = preload("res://scenes/Particles/explosion.tscn")
+@onready var explosive_scene = preload("res://scenes/rocket.tscn")
+
+var time_since_last_action: float = seconds_between_shots
+var i_target: int = 0
+
+var is_open: bool = false
+var is_hitable: bool = false
+
+func _physics_process(delta: float) -> void:
+	time_since_last_action += delta
+	
+	if is_open and time_since_last_action >= seconds_being_open:
+		AnimPlayer.play("Close")
+		time_since_last_action = 0
+	elif !is_open and time_since_last_action >= seconds_between_shots:
+		AnimPlayer.play("Open")
+		time_since_last_action = 0
+		
+		set_hitable(true)
+
+
+func launch_explosive():
+	if targets.size() == 0:
+		printerr(self.name + " in " + get_parent().name + ": No targets.")
+		return
+	var explosive = explosive_scene.instantiate()
+	explosive.target = targets[i_target].global_position
+	explosive.speed = explosive_speed
+	explosive.incline = incline
+	explosive.global_position = global_position
+	get_parent().add_child(explosive)
+	# cycle targets
+	i_target = (i_target + 1) % targets.size()
+
+func set_hitable(new_hitable: bool):
+	is_hitable = new_hitable
+	
+	# toggle wether to bounce or stop the bullet
+	#set_collision_layer_value(1, !is_hitable)
+	#set_collision_layer_value(2, is_hitable)
+
+func on_hit():
+	if is_hitable:
+		print("explosive thrower hit")
+		queue_free()
+	else:
+		print("explosive thrower blocked")
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Open":
+		launch_explosive()
+		is_open = true
+		time_since_last_action = 0
+	else:
+		is_open = false
+		set_hitable(false)
+		time_since_last_action = 0
