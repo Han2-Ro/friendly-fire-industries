6affec197e2f6d3b3dcddc2194f2dc12b54c83a1
anybuddy9001 <fuaw.gc@lst.de>
2025-02-21 00:17:24 +0100

    laser door + colors button

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,6 +1,16 @@
 extends StaticBody3D
 
+@export var color: Color = Color.RED
+
 signal button_hit
+
+func _ready() -> void:
+	# set color
+	var mat = StandardMaterial3D.new()
+	mat.albedo_color = color
+	mat.emission = color
+	
+	$MeshInstance3D.material_override = mat
 
 func on_hit():
 	print("button hit")
--- scripts/door.gd
+++ scripts/door.gd
@@ -1,15 +1,31 @@
 extends Node3D
 
-@onready var coll = $Area3D/CollisionShape3D
+@export var color: Color = Color.RED
 
 var is_open: bool = false
 
+func _ready() -> void:
+	# set color
+	var mat = StandardMaterial3D.new()
+	mat.albedo_color = color
+	mat.emission_enabled = true
+	mat.emission_energy_multiplier = 3
+	mat.emission = color
+	
+	$"Laser-Emitters/LaserEmitters-Far/LaserEmittersColor".material_override = mat
+	$"Laser-Emitters/LaserEmitters-Near/LaserEmittersColor".material_override = mat
+	$"Laser-Bars/Top".material_override = mat
+	$"Laser-Bars/Mid".material_override = mat
+	$"Laser-Bars/Bot".material_override = mat
+
 func toggle_door() -> void:
 	print("toggle laser-door")
-	# ToDo hide lasers
+	
+	var coll = $Area3D/CollisionShape3D
 	if is_open:
 		coll.disabled = false
 	else:
+		# ToDo hide lasers
 		coll.disabled = true
 	is_open = not is_open
 
