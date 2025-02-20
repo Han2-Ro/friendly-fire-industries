0078537fb5eb72fd2be1ce9168dc373808860698
anybuddy9001 <fuaw.gc@lst.de>
2025-02-20 17:08:06 +0100

    button intro map logic + door rework start

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,11 +1,22 @@
 extends Node3D
 
+@onready var coll = $Area3D/CollisionShape3D
+
 var is_open: bool = false
 
 func toggle_door() -> void:
-	print("toggle door")
+	print("toggle laser-door")
+	# ToDo hide lasers
 	if is_open:
-		$AnimationPlayer.play_backwards("open_door")
+		coll.disabled = true
 	else:
-		$AnimationPlayer.play("open_door")
+		coll.disabled = false
 	is_open = not is_open
+
+#func toggle_door() -> void:
+	#print("toggle door")
+	#if is_open:
+		#$AnimationPlayer.play_backwards("open_door")
+	#else:
+		#$AnimationPlayer.play("open_door")
+	#is_open = not is_open
