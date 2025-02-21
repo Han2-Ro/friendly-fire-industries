14f9bc4a6daeb9f6719faf604b408d0c4121ef82
anybuddy9001 <fuaw.gc@lst.de>
2025-02-21 22:19:51 +0100

    preload

---
+++ scripts/preload.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+func _ready() -> void:
+	preload("res://scenes/button.tscn")
+	preload("res://scenes/bullet.tscn")
+	preload("res://scenes/turret.tscn")
+	preload("res://scenes/particles/explosion.tscn")
+	preload("res://scenes/player.tscn")
