268f88f147d49e9bdc48c9bff9f124f8191ba594
leog312 <leo.gallwitz@icloud.com>
2025-02-21 15:08:18 +0100

    audio fix

--- scripts/audio_stream_player.gd
+++ scripts/audio_stream_player.gd
@@ -1,7 +1,7 @@
 extends AudioStreamPlayer
 
 const level_music = preload("res://assets/music/Future-Industry-1.mp3")
-
+	
 func _play_music(music: AudioStream, volume = -10.0):
 	if stream == music:
 		print("playing")
@@ -14,4 +14,3 @@ func _play_music(music: AudioStream, volume = -10.0):
 func play_music_level():
 	_play_music(level_music)
 	print("playing")
-	
