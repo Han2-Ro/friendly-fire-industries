c7f48076dbd3d31514de2fc36b9f4d807c15de53
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-02-22 21:15:40 +0100

    add sound to button in endscreen

--- scripts/ui_end_screen.gd
+++ scripts/ui_end_screen.gd
@@ -1,15 +1,18 @@
 extends Control
 
+@onready var hover_audio_player: AudioStreamPlayer = $HoverAudioPlayer
+@onready var click_audio_player: AudioStreamPlayer = $ClickAudioPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameState.current_level = 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_main_menu_button_pressed() -> void:
+	play_on_mouse_click()
 	get_tree().change_scene_to_file("res://scenes/levels/main_menu/main_menu.tscn")
+
+func play_on_mouse_entered() -> void:
+	hover_audio_player.play()
+	
+func play_on_mouse_click() -> void:
+	click_audio_player.play()
